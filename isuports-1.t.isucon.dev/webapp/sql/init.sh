a635dafcda5b3dea4162abf1cc80795b4fc2140d
ShotaKitazawa <skitazawa1121@gmail.com>
2022-07-23 15:15:34 +0900

    update

--- isuports-1.t.isucon.dev/webapp/sql/init.sh
+++ isuports-1.t.isucon.dev/webapp/sql/init.sh
@@ -33,8 +33,6 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < player.sql
 
-mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" --host "$ISUCON_DB_HOST" --port "$ISUCON_DB_PORT" "$ISUCON_DB_NAME" \
-  -e 'DELETE FROM player_score;'
 mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
