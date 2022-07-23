b9f3b96056e1c2d89d44e390c8ef8052f3a64ebc
ShotaKitazawa <skitazawa1121@gmail.com>
2022-07-23 11:31:18 +0900

    add/remove index

--- isuports-1.t.isucon.dev/webapp/sql/init.sh
+++ isuports-1.t.isucon.dev/webapp/sql/init.sh
@@ -16,6 +16,14 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
 
+# add/remove index
+mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" --host "$ISUCON_DB_HOST" --port "$ISUCON_DB_PORT" "$ISUCON_DB_NAME" \
+  -e 'ALTER TABLE visit_history ADD INDEX tenant_competition_player (tenant_id,competition_id,player_id);'
+
+mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" --host "$ISUCON_DB_HOST" --port "$ISUCON_DB_PORT" "$ISUCON_DB_NAME" \
+  -e 'DROP INDEX tenant_id_idx ON visit_history;'
+
+
 # SQLiteのデータベースを初期化
 rm -f ../tenant_db/*.db
 cp -r ../../initial_data/*.db ../tenant_db/
