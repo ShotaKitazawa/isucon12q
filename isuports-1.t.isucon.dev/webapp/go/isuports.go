e31aa61dbd5e82734c83a8a9046dba492dd02671
ShotaKitazawa <skitazawa1121@gmail.com>
2022-07-23 13:51:25 +0900

    for debug

--- isuports-1.t.isucon.dev/webapp/go/isuports.go
+++ isuports-1.t.isucon.dev/webapp/go/isuports.go
@@ -905,7 +905,7 @@ func playerDisqualifiedGoroutine() {
 			// TODO: DB が1つになったら bulk insert する
 			for _, data := range datas {
 				//debug
-				fmt.Println(data.playerId)
+				fmt.Printf("playerId: %s\n", data.playerId)
 
 				tenantDB, err := connectToTenantDB(data.tenantId)
 				if err != nil {
