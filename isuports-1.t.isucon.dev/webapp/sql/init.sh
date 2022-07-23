16547f2c2962bd2f4432674f70a18280bc2e6db3
ShotaKitazawa <skitazawa1121@gmail.com>
2022-07-23 15:18:58 +0900

    update

--- isuports-1.t.isucon.dev/webapp/sql/init.sh
+++ isuports-1.t.isucon.dev/webapp/sql/init.sh
@@ -49,5 +49,5 @@ mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" --host "$ISUCON_DB_HOST" --por
 
 
 # SQLiteのデータベースを初期化
-rm -f ../tenant_db/*.db
-cp -r ../../initial_data/*.db ../tenant_db/
+#rm -f ../tenant_db/*.db
+#cp -r ../../initial_data/*.db ../tenant_db/
